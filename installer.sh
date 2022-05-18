7d7f0db3fd0218793ed995d650a802e23bb38c6d
EdwardFetch <75830825+FreshPopcorn42@users.noreply.github.com>
2022-05-18 13:37:06 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,46 @@ echo \n
 echo "Which script may I download for you dearest? 
 Enter one after one."
 read choice[]
-
 read choice
 
-/home/$USERNAME/Downloads
+for item in $choice[]
+do
+  if [$item == 'update']
+  then
+    git pull
+  elif [$item == 'updatePlus']
+  then
+    git pull
+  elif [$item == 'newOSversion']
+  then
+    git pull
+  elif [$item == 'brightness']
+  then
+    git pull
+  elif [$item == 'SysToFile']
+  then
+    git pull
+  elif [$item == 'Schneewittchen']
+  then
+    git pull
+  elif [$item == 'ProxyDoe']
+  then
+    git pull
+  elif [$item == 'ProtocolZeroControl']
+  then
+    git pull
+  elif [$item == 'InstallSetup']
+  then
+    git pull
+  elif [$item == 'Flatpak']
+  then
+    git pull
+  elif [$item == 'Firefox']
+  then
+    git pull
+  elif [$item == 'CheckIPs']
+  then
+    git pull
+  fi
+
+rm -rf /home/$USERNAME/Downloads/Readme.md
