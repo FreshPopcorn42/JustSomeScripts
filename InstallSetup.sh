445d932cbc794ef4f61d861f2b28ca9c270d7086
EdwardFetch <75830825+FreshPopcorn42@users.noreply.github.com>
2022-05-31 18:52:36 +0000

    Update InstallSetup.sh

--- InstallSetup.sh
+++ InstallSetup.sh
@@ -24,6 +24,8 @@ then
 	dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm >> $logfile
  
 	dnf update -y >> $logfile
+	sudo dnf install plan -y
+	sudo dnf install kalendar -y
 	
 	# add flathub
 	pr "Adding Flathub"
