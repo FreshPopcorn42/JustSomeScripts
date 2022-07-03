e93ea585a689960a7166b07ad13cb2ee38757b61
EdwardFetch <75830825+FreshPopcorn42@users.noreply.github.com>
2022-07-03 15:11:08 +0000

    Update fileFinder.sh

--- fileFinder.sh
+++ fileFinder.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+whatToDoe = $wtd
+
+if [$wtd == "-f"]
+then
+
+#get file name
+  echo "Enter name of file"
+  read name
+elif [$wtd == "-d"]
+
+#actually find file
+cat / | grep $name.*
+then
+
+#get directory name
+  echo "Enter name of directory"
+  read name
+  
+  #actually find directory
+  cat / | grep /$name
+fi
